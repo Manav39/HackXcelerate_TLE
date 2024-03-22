97ac9b939c39ffc460d9aa66099aced412eaf1a2
Manav39 <Manav39>
2024-03-22 14:38:04 +0530

    Adding admin tabs

--- Apps/Screens/AdminScreen.jsx
+++ Apps/Screens/AdminScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import AdminNavigation from "../Navgations/AdminNavigation";
 
 export default function AdminScreen() {
   return (
-    <View>
-      <Text>AdminScreen</Text>
+    <View className="flex-1">
+      <AdminNavigation />
     </View>
   );
 }
