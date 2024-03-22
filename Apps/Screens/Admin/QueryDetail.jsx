352053f4b0e721b6e8444709cb8b7d19f3aa60ab
Sneha Menat <snehamenat029@gmail.com>
2024-03-23 01:32:55 +0530

    "errors solved for loginscreen"

--- Apps/Screens/Admin/QueryDetail.jsx
+++ Apps/Screens/Admin/QueryDetail.jsx
@@ -1,21 +1,3 @@
-// import { View, Text } from "react-native";
-// import React from "react";
-// import { useRoute } from "@react-navigation/native";
-
-// export default function QueryDetail() {
-//   const { params } = useRoute();
-//   return (
-//     <View>
-//       <Text className="mt-20 text-[30px] font-bold text-center">
-//         {params.item.subject}
-//       </Text>
-//       <Text className="p-4">{params.item.description}</Text>
-//       <Text className="font-bold text-[20px] ml-2">
-//         Queried by : {params.item.useremail}
-//       </Text>
-//     </View>
-//   );
-// }
 
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  Alert,
-  StyleSheet,
-  Image,
-} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { db } from "../firebase";
 import { getDocs, collection, query, where } from "firebase/firestore";
 import { useAuth } from "../context";
-import React, { useState } from 'react'
 import {
   View,
   Text,
@@ -22,10 +12,6 @@ import {
   StyleSheet,
   Image,
 } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { db } from '../firebase'
-import { getDocs, collection, query, where } from 'firebase/firestore'
-import { useAuth } from '../context'
 
 export default function LoginScreen() {
   const { setRole, setUserName, setEmail } = useAuth()
