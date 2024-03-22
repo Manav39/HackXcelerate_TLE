39bb2aa8165d953136b15068f2abf6882ce68509
Manav39 <Manav39>
2024-03-22 23:53:17 +0530

    Changes in styling of verify

--- Apps/Screens/Admin/verificationBox.jsx
+++ Apps/Screens/Admin/verificationBox.jsx
@@ -2,43 +2,42 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-const RequestBox = ({ businessName, seller, onPress }) => {
+const RequestBox = ({ businessName, seller, url, onPress }) => {
   return (
     <TouchableOpacity
       style={styles.container}
       onPress={onPress}
       className="flex flex-row"
     >
-<<<<<<< HEAD
       <View style={{ flexDirection: "row", height: 100 }}>
         <Image
-          source={require("../../../assets/images/signup.png")}
+          source={{ uri: url }}
           style={styles.image}
+          className="object-contain mr-7"
         />
         <View style={{ flexDirection: "column", marginTop: 20 }}>
           <Text style={{ fontWeight: "bold", fontSize: 20 }}>
             {businessName}
           </Text>
           <Text style={{ fontSize: 15 }}>Requested By : {seller} </Text>
-=======
-      <View style={{ flexDirection: 'row', height:100, justifyContent: 'center', alignItems: 'center' }}>
-        <Image source={require("../../../assets/images/signup.png")} style={styles.image} />
-        <View style={{ flexDirection: 'column' }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{businessName}</Text>
-          <Text style={{ fontSize: 15 }}>Requested By - ....by seller </Text>
->>>>>>> 4e5943dc96a49cba15c6c4d725d632dcf81baf5a
         </View>
         <View
           style={{
             flexDirection: "column",
-            marginTop: 20,
+            marginTop: 25,
+            marginLeft: 20,
             backgroundColor: "#007bff",
             height: 25,
             width: 25,
             borderRadius: 20,
           }}
         >
-          <Ionicons name="chevron-forward" size={24} color="white" />
+          <Ionicons
+            className="ml-10"
+            name="chevron-forward"
+            size={24}
+            color="white"
+          />
         </View>
       </View>
     </TouchableOpacity>
