0b06608226791ce582ba0c1b2f135c66c4a0801b
Sneha Menat <snehamenat029@gmail.com>
2024-03-22 22:57:12 +0530

    "queries and report ui interface"

--- Apps/Screens/Admin/QueryBox.jsx
+++ Apps/Screens/Admin/QueryBox.jsx
@@ -9,15 +9,29 @@ const QueryBox = ({ QueryName, onPress }) => {
       onPress={onPress}
       className="flex flex-row"
     >
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{QueryName}</Text>
-      <Text style={{ fontSize: 15 }}>Queried By - ....by user </Text>
-      {/* <Ionicons name="chevron-forward" size={24} color="black" /> */}
+      
+      <View style={{flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+        <View style={{justifyContent:'center', marginTop:12}}>
+        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{QueryName}</Text>
+        <View style={{flex:1, flexDirection:'row'}}>
+        <Text style={{ fontSize: 15, marginRight: 10 }}>Queried By - ....by user </Text>
+        <Text style={{color:'grey'}}>1 min ago</Text>
+        </View>
+        </View>
+
+        <View style={{marginLeft:55, height:25, width:25 , padding:4, alignItems:'flex-end',  borderRadius:20, backgroundColor:'#007bff'}}>
+        <Ionicons name="chevron-forward" size={18} color="#ffffff" />
+        </View>
+      </View>
+      
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    borderWidth:2,
+    borderColor:'#007bff',
     flexDirection: 'col',
     alignItems: 'flex-start',
     justifyContent: 'space-between',
@@ -26,10 +40,11 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     borderRadius: 10,
     marginBottom: 15,
+    height:100,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
